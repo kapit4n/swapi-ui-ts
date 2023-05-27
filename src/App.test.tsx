4b26765b8621fcc517e618fbeb70ed223676aeef
Luis Arce <luis.arce22@gmail.com>
2023-05-27 02:24:05 -0400

    Fix basic unit  test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+jest.mock('axios');
+
+test('renders people button', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/People/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders Popular button', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Popular/i);
   expect(linkElement).toBeInTheDocument();
 });
